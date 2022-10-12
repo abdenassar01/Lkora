e6e52cb0ae8f309c420b0d2c3c5dd20f9c75ac3a
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-12 18:24:01 +0100

    add styled components and create screens files

---
+++ app/screens/tabs/main/MainScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function MainScreen() {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
---
+++ app/screens/tabs/matches/Matches.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Matches() {
+  return (
+    <View>
+      <Text>Matches</Text>
+    </View>
+  )
+}
