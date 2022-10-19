2f2b647e641f0fedd5170343fa2d793085f6f55b
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-19 16:03:30 +0100

    add horizontal section changing and refactor✂🤯

--- app/screens/tabs/main/mainStack/eventDetails/statistics/Statistics.tsx
+++ app/screens/tabs/main/mainStack/eventDetails/statistics/Statistics.tsx
@@ -1,33 +1,43 @@
-import { Label, StatisticsItem, StatisticsWrapper, Value } from './styles/Styles'
+import { useEffect, useState } from 'react'
+import { COLOR } from '../../../../../../assets/color';
+import Stats from './sections/Stats';
+import { ButtonLabel, Container, NavBar, NavItem } from './styles/Styles'
 
 export default function Statistics() {
+
+  const [ section, setSection ] = useState<string>("stats");
+
+  useEffect(() => {
+    console.log(section)
+  },[ section ])
+
   return (
-    <StatisticsWrapper>
-      <StatisticsItem>
-        <Value>35%</Value>
-        <Label>Possition</Label>
-        <Value>65%</Value>
-      </StatisticsItem>
-      <StatisticsItem>
-        <Value>35%</Value>
-        <Label>Possition</Label>
-        <Value>65%</Value>
-      </StatisticsItem>
-      <StatisticsItem>
-        <Value>35%</Value>
-        <Label>Possition</Label>
-        <Value>65%</Value>
-      </StatisticsItem>
-      <StatisticsItem>
-        <Value>35%</Value>
-        <Label>Possition</Label>
-        <Value>65%</Value>
-      </StatisticsItem>
-      <StatisticsItem>
-        <Value>35%</Value>
-        <Label>Possition</Label>
-        <Value>65%</Value>
-      </StatisticsItem>
-    </StatisticsWrapper>
+    <Container style={{ elevation: 3 }}>
+      <NavBar>
+        <NavItem onPress={ () => setSection("stats") } 
+          style={{ 
+            backgroundColor: section === "stats" ? COLOR.main : COLOR.background,
+          }}
+        >
+          <ButtonLabel style={{ color: section === "stats" ? COLOR.text : COLOR.helperText }}>Stats</ButtonLabel>
+        </NavItem>
+        <NavItem onPress={ () => setSection("lineups") } 
+          style={{ 
+            backgroundColor: section === "lineups" ? COLOR.main : COLOR.background,
+          }}
+        >
+          <ButtonLabel style={{ color: section === "lineups" ? COLOR.text : COLOR.helperText }}>Line-up</ButtonLabel>
+        </NavItem>
+        <NavItem onPress={ () => setSection("summary") } 
+          style={{ 
+            backgroundColor: section === "summary" ? COLOR.main : COLOR.background,
+          }}
+        >
+          <ButtonLabel style={{ color: section === "summary" ? COLOR.text : COLOR.helperText }}>Summary</ButtonLabel>
+        </NavItem>
+      </NavBar>
+      <Stats />
+    </Container>
+    
   )
 }
--- app/screens/tabs/main/mainStack/eventDetails/statistics/styles/Styles.ts
+++ app/screens/tabs/main/mainStack/eventDetails/statistics/styles/Styles.ts
@@ -1,12 +1,16 @@
-import { ScrollView, View } from 'react-native';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
 import styled from "styled-components";
 import { MainText } from '../../../../../../../assets/text/Text';
 
-export const StatisticsWrapper = styled(ScrollView)`
+export const Container = styled(View)`
     margin: 10px;
     padding: 5px;
     background-color: white;
     border-radius: 10px;
+`
+
+export const StatisticsWrapper = styled(ScrollView)`
+
 `
 
 export const StatisticsItem = styled(View)`
@@ -22,4 +26,20 @@ export const Value = styled(MainText)`
 
 export const Label = styled(MainText)`
     font-size: 20px;
+`
+
+export const NavBar = styled(View)`
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 3px 0;
+`
+
+export const NavItem = styled(TouchableOpacity)`
+    padding: 3px 15px;
+    border-radius: 10px;
+`
+
+export const ButtonLabel = styled(MainText)`
+    font-size: 15px;
+    font-weight: 600;
 `
