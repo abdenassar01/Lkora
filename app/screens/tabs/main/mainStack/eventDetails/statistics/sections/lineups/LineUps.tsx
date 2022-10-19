b75522da45e6907ae338046fdaf23c5ab674d278
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-19 19:38:20 +0100

    style lineups in event details screen 🎨 ⚜

--- app/screens/tabs/main/mainStack/eventDetails/statistics/sections/lineups/LineUps.tsx
+++ app/screens/tabs/main/mainStack/eventDetails/statistics/sections/lineups/LineUps.tsx
@@ -3,11 +3,61 @@ import { Avatar, Heading, LineUpWrapper, Name, PlayerWrapper, Position, Rating, 
 
 export default function LineUps() {
   return (
-    <LineUpWrapper>
+    <LineUpWrapper contentContainerStyle={{ alignItems: 'center' }}>
       <Heading>Home</Heading>
       <PlayerWrapper>
         <Position>G</Position>
         <ShirtNumber>1</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>LF</Position>
+        <ShirtNumber>25</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>L</Position>
+        <ShirtNumber>5</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>RB</Position>
+        <ShirtNumber>3</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <Heading>Away</Heading>
+      <PlayerWrapper>
+        <Position>G</Position>
+        <ShirtNumber>1</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>LF</Position>
+        <ShirtNumber>25</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>L</Position>
+        <ShirtNumber>5</ShirtNumber>
+        <Name>Alison Baker</Name>
+        <Rating>7.2</Rating>
+        <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
+      </PlayerWrapper>
+      <PlayerWrapper>
+        <Position>RB</Position>
+        <ShirtNumber>3</ShirtNumber>
         <Name>Alison Baker</Name>
         <Rating>7.2</Rating>
         <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/243609/image" }}></Avatar>
