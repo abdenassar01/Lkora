859a1509a305a0bcb1f53bbe40ee9a1bfb49fa96
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-20 15:24:32 +0100

    adding axios and start first api call 🤙

--- app/screens/tabs/main/mainStack/live/Live.tsx
+++ app/screens/tabs/main/mainStack/live/Live.tsx
@@ -1,7 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import { LiveSlider } from "../styles/Styles";
 import LiveCard from "./card/LiveCard";
 
 export default function Live() {
+
+  const { data, isFetching, error } = useQuery(["fetch live events"], async () => {
+
+  })
+
   return (
     <LiveSlider horizontal>
       <LiveCard />
