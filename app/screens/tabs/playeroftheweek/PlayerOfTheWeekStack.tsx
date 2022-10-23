4d57d1795961fff5e093c293151c3b28f152e88a
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-23 11:03:33 +0100

    setting up screens stack 🧬 📱

--- app/screens/tabs/playeroftheweek/PlayerOfTheWeekStack.tsx
+++ app/screens/tabs/playeroftheweek/PlayerOfTheWeekStack.tsx
@@ -1,7 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
 
 export default function PlayerOfTheWeekStack() {
+
+  const Stack = createStackNavigator();
+
   return (
     <View style={{ flex: 1 }}>
       <Text>PlayerOfTheWeekStack</Text>
---
+++ app/screens/tabs/playeroftheweek/stack/playerlist/PlayersList.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PlayersList() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>PlayersList</Text>
+    </View>
+  )
+}
