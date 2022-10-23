8a36a2a2d76716cdd1f2619276d0dc4ca371b647
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-23 22:30:25 +0100

    adding styles and making header leyout 🌲🚙

--- app/screens/tabs/playeroftheweek/stack/playerdetails/PlayerDetails.tsx
+++ app/screens/tabs/playeroftheweek/stack/playerdetails/PlayerDetails.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 import React from 'react'
+import { COLOR } from '../../../../../assets/color'
+import { BackIcon, Header, HeaderTitle, PlayerDetailsWrapper } from './styles/Styles'
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 export default function PlayerDetails() {
+
+  const navigation: any = useNavigation()
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>PlayerDetails</Text>
-    </View>
-  )
+    <PlayerDetailsWrapper>
+      <Header>
+        <BackIcon onPress={ () => navigation.goBack() }>
+          <Ionicons name="chevron-back-circle-sharp" size={ 35 } color={ COLOR.main } />
+        </BackIcon>
+        <HeaderTitle>Ossmane Dembele</HeaderTitle>
+      </Header>
+    </PlayerDetailsWrapper>
+  ) 
 }
--- app/screens/tabs/playeroftheweek/stack/playerdetails/styles/Styles.ts
+++ app/screens/tabs/playeroftheweek/stack/playerdetails/styles/Styles.ts
@@ -1,6 +1,56 @@
-import { View } from "react-native";
+import { Image, ScrollView, View, Pressable } from "react-native";
 import styled from "styled-components";
+import { COLOR } from "../../../../../../assets/color";
+import { MainText } from "../../../../../../assets/text/Text";
 
-export const PlayerDetailsWrapper = styled(View)`
+export const PlayerDetailsWrapper = styled(ScrollView)`
+    background-color: ${ COLOR.background };
+`
+
+export const Header = styled(View)`
+
+`
+
+export const BackIcon = styled(Pressable)`
+
+`
+
+export const HeaderTitle = styled(MainText)`
+
+`
+
+export const HeroSection = styled(View)`
+
+`
+
+export const Avatar = styled(Image)`
+
+`
+
+export const Rating = styled(View)`
+
+`
+
+export const RatingText = styled(MainText)`
+
+`
+
+export const Paragraph = styled(MainText)`
+
+`
+
+export const Stats = styled(View)`
+
+`
+
+export const Heading = styled(MainText)`
+
+`
+
+export const StatsItem = styled(View)`
+
+`
+
+export const Value = styled(MainText)`
 
 `
