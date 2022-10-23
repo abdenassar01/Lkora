a63edd7b2d661b05a95733b82b1e64b6150f3727
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-23 11:36:29 +0100

    styling player card 🤵 🥇

--- app/screens/tabs/playeroftheweek/stack/playerlist/PlayersList.tsx
+++ app/screens/tabs/playeroftheweek/stack/playerlist/PlayersList.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Player, Left, PlayerAvatar, PlayerListWrapper, PlayerName, PressablePlayer, Rating, TeamName, RatingWrapper, Heading } from './styles/Style'
 
 export default function PlayersList() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>PlayersList</Text>
-    </View>
+    <PlayerListWrapper >
+      <Heading>Top Players Of The Week</Heading>
+
+      {/*  player */}
+      <PressablePlayer>
+        <Player style={{ elevation: 5 }}>
+          <Left>
+            <PlayerAvatar source={{ uri: `https://api.sofascore.app/api/v1/player/100386/image` }}></PlayerAvatar>
+            <PlayerName>Jadon Sancho</PlayerName>
+          </Left>
+          <TeamName>Liverpool</TeamName>
+          <RatingWrapper>
+            <Rating>6.2</Rating>
+          </RatingWrapper>
+        </Player>
+      </PressablePlayer>
+
+      {/*  */}
+      <PressablePlayer>
+        <Player style={{ elevation: 5 }}>
+          <Left>
+            <PlayerAvatar source={{ uri: `https://api.sofascore.app/api/v1/player/100386/image` }}></PlayerAvatar>
+            <PlayerName>Jadon Sancho</PlayerName>
+          </Left>
+          <TeamName>Liverpool</TeamName>
+          <RatingWrapper>
+            <Rating>6.2</Rating>
+          </RatingWrapper>
+        </Player>
+      </PressablePlayer>
+    </PlayerListWrapper>
   )
 }
