9bc2e4d54f9e24ffb161ca2bc4fe041605336791
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-24 18:51:11 +0100

    adding standing Styling 🦸🏻‍♀️ 🕹

--- app/screens/tabs/botola/stack/fixtures/Fixtures.tsx
+++ app/screens/tabs/botola/stack/fixtures/Fixtures.tsx
@@ -1,10 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { BigHeading, BotolaFixWrapper, Column, Row, Table, TieBreakingRule, TieBreakingRuleHeading, TieBreakingRuleText } from './styles/Styles'
 
 export default function Fixtures() {
   return (
-    <View>
-      <Text>Fixtures</Text>
-    </View>
+    <BotolaFixWrapper contentContainerStyle={{ justifyContent: 'center' }}>
+      <BigHeading>Botola Pro</BigHeading>
+      <TieBreakingRule>
+        <TieBreakingRuleHeading>Tie Breaking Rule</TieBreakingRuleHeading>
+        <TieBreakingRuleText>
+         In the event that two (or more) teams finish with an equal number of points, the following rules break the tie:  1. Head-to-head games between the teams concerned   1a. Points total  1b. Goal difference  1c. Goals scored 2. Goal difference 3. Goals scored
+        </TieBreakingRuleText>
+      </TieBreakingRule>
+      <Table>
+        <Row>
+            <Column style={{ width: 20 }}>#</Column>
+            <Column style={{ width: 170 }}>Team</Column>
+            <Column style={{ width: 20 }}>P</Column>
+            <Column style={{ width: 20, color: "white", backgroundColor: "#07c05a" }}>W</Column>
+            <Column style={{ width: 20, color: "white", backgroundColor: "#f8b600" }}>D</Column>
+            <Column style={{ width: 20, color: "white", backgroundColor: "#e30044" }}>L</Column>
+            <Column style={{ width: 20 }}>+</Column>
+            <Column style={{ width: 20 }}>-</Column>
+            <Column style={{ width: 30 }}>Pts</Column>
+        </Row> 
+        <Row>
+            <Column style={{ width: 20 }}>1</Column>
+            <Column style={{ width: 170 }}>Raja Club Athletic</Column>
+            <Column style={{ width: 20 }}>6</Column>
+            <Column style={{ width: 20 }}>1</Column>
+            <Column style={{ width: 20 }}>3</Column>
+            <Column style={{ width: 20 }}>2</Column>
+            <Column style={{ width: 20 }}>5</Column>
+            <Column style={{ width: 20 }}>6</Column>
+            <Column style={{ width: 30 }}>6</Column>
+        </Row>
+      </Table>
+    </BotolaFixWrapper>
   )
 }
---
+++ app/screens/tabs/botola/stack/fixtures/styles/Styles.ts
@@ -0,0 +1,59 @@
+import { ScrollView, View } from "react-native";
+import styled from "styled-components";
+import { COLOR } from "../../../../../../assets/color";
+import { MainText } from "../../../../../../assets/text/Text";
+
+export const BotolaFixWrapper = styled(ScrollView)`
+    background-color: ${ COLOR.background };   
+`
+
+export const BigHeading = styled(MainText)`
+    font-size: 25px;
+    padding: 10px;
+    color: ${ COLOR.main };
+    text-align: center;
+`
+
+export const TieBreakingRule = styled(View)`
+    margin: 10px;
+    padding: 10px;
+    background-color: ${ COLOR.text }; 
+    align-items: center;
+    border-radius: 10px;
+    elevation: 5;
+`
+
+export const TieBreakingRuleHeading = styled(MainText)`
+    text-align: center;
+    color: ${ COLOR.main };
+    border-bottom-style: solid;
+    border-bottom-color: ${ COLOR.main };
+    border-bottom-width: 2px;
+    border-bottom-left-radius: 50px;
+    border-bottom-right-radius: 50px;
+    width: 60%;
+    font-size: 16px;
+    margin-bottom: 10px;
+`
+
+export const TieBreakingRuleText = styled(MainText)`
+    text-align: center;
+`
+
+export const Table = styled(View)`
+    margin: 10px;
+    padding: 10px;
+    background-color: ${ COLOR.text }; 
+    border-radius: 10px;
+    elevation: 5;
+`
+
+export const Row = styled(View)`
+    flex-direction: row;
+`
+
+export const Column = styled(MainText)`
+    padding:  2px;
+    border: 1px solid ${ COLOR.main }; 
+    text-align: center;
+`
