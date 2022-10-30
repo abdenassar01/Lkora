ed5eace6ab7f03b6b43656b424e1b1fcc91c2bd2
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-30 12:56:10 +0100

    adding tournomants data canvas 🌆

---
+++ app/assets/tournomants.ts
@@ -0,0 +1,27 @@
+type Tournomant = {
+    id: Number,
+    label: string
+}
+
+export const Tournomants: Tournomant[] = [
+    {
+        id: 1,
+        label: "Premier League"
+    },
+    {
+        id: 1,
+        label: "Premier League"
+    },
+    {
+        id: 1,
+        label: "Premier League"
+    },
+    {
+        id: 1,
+        label: "Premier League"
+    },
+    {
+        id: 1,
+        label: "Premier League"
+    }
+]
