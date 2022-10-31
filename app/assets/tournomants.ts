bf4a96bcf375cc2aa8a8eb4a5f6f7bdb2af2e31f
abdenassar01 <abdenassaramimi99@gmail.com>
2022-10-31 15:06:43 +0100

    update tournomants data 🗃 🌲

--- app/assets/tournomants.ts
+++ app/assets/tournomants.ts
@@ -11,23 +11,38 @@ export const TOURNOMANTS: Tournomant[] = [
         seasonId: 41886,
     },
     {
-        id: 2,
+        id: 937,
         label: "El Botola",
         seasonId: 45552,
     },
     {
-        id: 162,
-        label: "Premier League",
-        seasonId: 41886,
+        id: 8,
+        label: "La Liga",
+        seasonId: 42409,
     },
     {
-        id: 17527,
-        label: "Premier League",
-        seasonId: 41886,
+        id: 35,
+        label: "Serie A",
+        seasonId: 42268,
     },
     {
-        id: 123,
-        label: "Premier League",
-        seasonId: 41886,
+        id: 22,
+        label: "1st Division",
+        seasonId: 40407,
+    },
+    {
+        id: 23,
+        label: "Serie A",
+        seasonId: 42415,
+    },
+    {
+        id: 24,
+        label: "League One",
+        seasonId: 42402,
+    },
+    {
+        id: 34,
+        label: "Ligue 1",
+        seasonId: 42273,
     }
 ]
