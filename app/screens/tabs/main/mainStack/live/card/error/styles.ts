aefac80151da63b2b67577a606c2c238845a3e9a
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-16 14:46:25 +0100

    styling error handler for live matches 🍅🐞

--- app/screens/tabs/main/mainStack/live/card/error/styles.ts
+++ app/screens/tabs/main/mainStack/live/card/error/styles.ts
@@ -3,9 +3,19 @@ import styled from 'styled-components';
 import { MainText } from '../../../../../../../assets/text/Text';
 
 export const LiveCartWrapper = styled(View)`
-    border: 1px solid red;
+    padding: 10px;
+    width: 300px;
+    height: 100px;
+    justify-content: center;
+    align-items: center;
+    margin: 5px;
+    margin-left: 30px;
+    border-radius: 10px;
+    background-color: white;
+    elevation: 5;
 `
 
 export const LiveText = styled(MainText)`
-
+    text-align: center; 
+    font-size: 16px;
 `
