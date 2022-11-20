59f2e835c5f90cafa1da74f37fef05cec920dd38
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-20 23:44:00 +0100

    adding globale error handler comp🙋‍♀️🥺

---
+++ app/utils/error/styles/Styles.tsx
@@ -0,0 +1,23 @@
+import { Image, View } from "react-native";
+import styled from "styled-components";
+import { COLOR } from "../../../assets/color";
+import { MainText } from "../../../assets/text/Text";
+
+export const Wrapper = styled(View)`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: ${ COLOR.text };
+`
+
+export const ErrorText = styled(MainText)`
+    font-size: 18px;
+    text-align: center;
+    padding: 20px;
+    color: ${ COLOR.main };
+`
+
+export const ErrorImage = styled(Image)`
+    width: 350px;
+    height: 250px;
+`
