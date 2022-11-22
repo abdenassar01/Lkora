d994a0d885a9ef40c4c5033af60684ee36f8ab82
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-23 00:16:44 +0100

    adding Team Type🏟💪

---
+++ types/team.ts
@@ -0,0 +1,47 @@
+type Category = {
+    name: string,
+    id: string
+}
+
+type Tournomant = {
+    name: string,
+    id: number
+}
+
+type Country = {
+    name: string
+}
+
+type Manager = {
+    name: string,
+    id: number,
+    shortName: string,
+    country: Country
+}
+
+type City = {
+    name: string,
+} 
+
+type Staduim = {
+    name: string,
+    capacity: number
+}
+
+type Venue  = {
+    city: City,
+    staduim: Staduim
+}
+
+type Team = {
+    name: string,
+    id: number,
+    shortName: string,
+    foundationDateTimestamp: number,
+    category: Category,
+    tournament: Tournomant,
+    manager: Manager,
+    venue: Venue
+}
+
+export default Team;
