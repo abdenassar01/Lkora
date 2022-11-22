513b898c0e1371d141111892080e39805ce79f10
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-22 22:10:13 +0100

    adding team details screen 🤯🙍‍♀️

--- app/screens/tabs/botola/stack/team/Team.tsx
+++ app/screens/tabs/botola/stack/team/Team.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Standing() {
+export default function Team() {
   return (
     <View>
-      <Text>Standing</Text>
+      <Text>Team</Text>
     </View>
   )
 }
