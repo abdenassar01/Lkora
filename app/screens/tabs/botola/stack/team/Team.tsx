9f06491d06720cbe513ad7364a665e986ed7f20b
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-22 23:53:30 +0100

    styling team details screen 👨‍✈️🏟

--- app/screens/tabs/botola/stack/team/Team.tsx
+++ app/screens/tabs/botola/stack/team/Team.tsx
@@ -1,10 +1,41 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import WithGoBackHeader from '../../../../../utils/header/withgoback/WithGoBackHeader'
+import { Avatar, Card, CardRightSide, Heading, Spacer, StaduimImage, TeamWrapper, Title, TournomantLogo } from './styles/Styles'
 
 export default function Team() {
   return (
-    <View>
-      <Text>Team</Text>
-    </View>
+    <TeamWrapper>
+        <WithGoBackHeader tournament='Arsonal'/>
+        <Card>
+            <Avatar source={{ uri: `https://api.sofascore.app/api/v1/team/42/image` }}></Avatar>
+            <CardRightSide>
+                <Heading>Arsonal</Heading>
+                <Heading>Premier League</Heading>
+                <Heading>1890</Heading>
+            </CardRightSide>
+            <TournomantLogo source={{ uri: `https://api.sofascore.app/api/v1/team/42/image` }}></TournomantLogo>
+        </Card>
+        <Title>Manager</Title>
+        <Card>
+            <Avatar source={{ uri: `https://api.sofascore.app/api/v1/team/42/image` }}></Avatar>
+            <CardRightSide>
+                <Heading>Mickel Artita</Heading>
+                <Heading>Espana</Heading>
+            </CardRightSide>
+        </Card>
+        <Title>Staduim</Title>
+        <Card>
+            <Spacer />
+            <CardRightSide>
+                <Heading>Emirates Stadium</Heading>
+                <Heading>60272</Heading>
+                <Heading>London</Heading>
+            </CardRightSide>
+            <StaduimImage source={{ uri: `https://api.sofascore.app/api/v1/team/42/image` }}></StaduimImage>
+            <Spacer />
+        </Card>
+        <Title>Players List</Title>
+    </TeamWrapper>
   )
 }
--- app/screens/tabs/botola/stack/team/styles/Styles.ts
+++ app/screens/tabs/botola/stack/team/styles/Styles.ts
@@ -1,31 +1,61 @@
 import { Image, ScrollView, View } from "react-native";
 import styled from "styled-components";
+import { COLOR } from "../../../../../../assets/color";
 import { MainText } from "../../../../../../assets/text/Text";
 
 export const TeamWrapper = styled(ScrollView)`
-
+    background-color: ${ COLOR.background };
 `
 
 export const Card = styled(View)`
-
+    margin: 10px;
+    flex-direction: row;
+    padding: 10px;
+    width: 95%;
+    border-radius: 10px;
+    background-color: ${ COLOR.text };
+    align-items: center;
+    elevation: 5;
 `
 
-export const Heading = styled(MainText)`
+export const TournomantLogo = styled(Image)`
+    width: 60px;
+    height: 60px;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    opacity: 0.4;
+`
 
+export const CardRightSide = styled(View)`
+    margin-left: 10px;
+` 
+
+export const Heading = styled(MainText)`
+    font-size: 14px;
+    color: ${ COLOR.textInverted };
 `
 
 export const Avatar = styled(Image)`
-
+    width: 80px;
+    height: 80px;
 `
 
 export const Title = styled(MainText)`
-
-`
-
-export const StaduimData = styled(View)`
-
+    text-align: center;
+    font-size: 20px;
+    margin: 5px 0;
+    color: ${ COLOR.main };
 `
 
 export const StaduimImage = styled(Image)`
+    width: 150px;
+    height: 80px;
+    position: absolute;
+    right: 10px;
+    border-radius: 10px;
+`
 
+export const Spacer = styled(View)`
+    height: 75px;
 `
