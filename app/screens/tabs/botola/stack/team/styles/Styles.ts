1150f9c48f480d5299a8790f8bf261b41c7d77c5
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-22 23:01:00 +0100

    adding required components 🕺🎹

--- app/screens/tabs/botola/stack/team/styles/Styles.ts
+++ app/screens/tabs/botola/stack/team/styles/Styles.ts
@@ -1,6 +1,31 @@
-import { ScrollView } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import styled from "styled-components";
+import { MainText } from "../../../../../../assets/text/Text";
 
 export const TeamWrapper = styled(ScrollView)`
+
+`
+
+export const Card = styled(View)`
+
+`
+
+export const Heading = styled(MainText)`
+
+`
+
+export const Avatar = styled(Image)`
+
+`
+
+export const Title = styled(MainText)`
+
+`
+
+export const StaduimData = styled(View)`
+
+`
+
+export const StaduimImage = styled(Image)`
 
 `
