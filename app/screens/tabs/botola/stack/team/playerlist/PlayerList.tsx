53c90de0a30c1c0e7e6290be2f8cf7d053e6535c
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-24 22:03:55 +0100

    add fitching functionality to player list comp🤪

--- app/screens/tabs/botola/stack/team/playerlist/PlayerList.tsx
+++ app/screens/tabs/botola/stack/team/playerlist/PlayerList.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import ErrorHandler from '../../../../../../utils/error/ErrorHandler';
 import { Avatar, CentreSection, LeftSection, Player, PlayerName, PlayersWrapper, RightSection, ShirtNumber, Spacer, TeamName } from './styles/Styles'
 
 type Props = {
@@ -5,20 +8,34 @@ type Props = {
 }
 
 export default function PlayerList({ id }: Props) {
+
+  const { data, isFetching, error } = useQuery("Get Team Details", async () => {
+    const result = await axios.get(`https://api.sofascore.app/api/v1/team/${ id }/players`);
+    return result?.data?.players;
+  })
+
+  if(isFetching) return <PlayerName>loading...</PlayerName>
+  if(error) return <ErrorHandler message='can not fetch team players' />
+
   return (
     <PlayersWrapper>
-      <Player onPress={ () => console.log("Player Clicked") }>
-        <LeftSection>  
-          <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/794839/image" }}></Avatar>
-          <CentreSection>
-              <PlayerName>Neymar Js</PlayerName>
-              <TeamName>Paris sant germain</TeamName>
-          </CentreSection>
-        </LeftSection>
-        <RightSection>
-            <ShirtNumber>10</ShirtNumber>
-        </RightSection>
-      </Player>
+      {
+        data?.map((player: any) => (
+          <Player onPress={ () => console.log("Player Clicked") }>
+            <LeftSection>  
+              <Avatar source={{ uri: "https://api.sofascore.app/api/v1/player/794839/image" }}></Avatar>
+              <CentreSection>
+                  <PlayerName>Neymar Js</PlayerName>
+                  <TeamName>Paris sant germain</TeamName>
+              </CentreSection>
+            </LeftSection>
+            <RightSection>
+                <ShirtNumber>10</ShirtNumber>
+            </RightSection>
+          </Player>
+        ))
+      }
+     
       <Spacer />
     </PlayersWrapper>
   )
