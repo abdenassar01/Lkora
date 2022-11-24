97e443c0775c92c344f8da1ab27a9912e7fb8772
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-24 23:02:07 +0100

    add player type🏃‍♀️🤘

---
+++ types/player.ts
@@ -0,0 +1,17 @@
+type Team = {
+    name: string,
+    id: number
+}
+
+type Player = {
+    id: number,
+    name: string,
+    position: string,
+    height: number,
+    jerseyNumber: string,
+    team: Team,
+}
+
+export type PlayerType = {
+    player: Player
+}
