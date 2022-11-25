aa8db07b978a8c06aa15be680c68ffb07b1ba169
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-25 23:53:33 +0100

    passing down the theme toggle handler and attaching it to press event👨‍🎓

--- app/utils/header/Header.tsx
+++ app/utils/header/Header.tsx
@@ -1,13 +1,22 @@
 import { HeaderBackground, HeaderWrapper, Heading } from "./styles/Styles";
 import Icon from "react-native-vector-icons/Ionicons";
 import { COLOR } from "../../assets/color";
+import { Pressable } from "react-native";
+import { useTheme } from "styled-components";
 
-export default function Header() {
+type Props = {
+  onThemeToggle: () => void
+}
+
+export default function Header({ onThemeToggle }: Props) {
+
   return (
     <HeaderBackground>  
       <HeaderWrapper>
           <Heading>Lkora</Heading>
-          <Icon name="football" size={ 30 } color={ COLOR.main }  />
+          <Pressable onPress={ onThemeToggle }>
+            <Icon name="football" size={ 30 } color={ COLOR.main }  />
+          </Pressable>
       </HeaderWrapper>
     </HeaderBackground>
   )
