2a1d5804a61faba2c2d0f96fe4d552717173aa91
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-25 23:12:19 +0100

    trying the dark theme 😊

--- app/assets/color.ts
+++ app/assets/color.ts
@@ -1,7 +1,15 @@
+//export const COLOR = {
+//    background: "#F7F7F7",
+//    main: "#FF2782",
+//    text: "white",
+//    textInverted: "#0d1c30",
+//    helperText: "#B6B6B6"
+//}
+
 export const COLOR = {
-    background: "#F7F7F7",
+    background: "#18191A",
     main: "#FF2782",
-    text: "white",
-    textInverted: "#0d1c30",
+    text: "#242526",
+    textInverted: "white",
     helperText: "#B6B6B6"
 }
