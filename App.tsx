5790608a9cf74696ab0056f593c6ef528e86d8cc
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-25 23:25:25 +0100

    fix themes implimentations 😊🤟

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
 } from 'react-native';
 import TabNavigator from './app/screens/tabs/TabNavigator';
 import { ThemeProvider } from 'styled-components';
-import theme from './app/assets/theme';
+import { light, dark } from './app/assets/theme';
 
 const queryClient = new QueryClient()
 
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <QueryClientProvider client={ queryClient }>
-        <ThemeProvider theme={ theme }>
+        <ThemeProvider theme={ light }>
           <StatusBar />
           <TabNavigator />
         </ThemeProvider>
