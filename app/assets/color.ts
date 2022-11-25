8b41230583817d446bf640b5a6660aadf64da044
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-25 23:17:37 +0100

    setting up themes ⚫⚪

--- app/assets/color.ts
+++ app/assets/color.ts
@@ -1,15 +1,7 @@
-//export const COLOR = {
-//    background: "#F7F7F7",
-//    main: "#FF2782",
-//    text: "white",
-//    textInverted: "#0d1c30",
-//    helperText: "#B6B6B6"
-//}
-
 export const COLOR = {
-    background: "#18191A",
-    main: "#FF2782",
-    text: "#242526",
-    textInverted: "white",
-    helperText: "#B6B6B6"
+   background: "#F7F7F7",
+   main: "#FF2782",
+   text: "white",
+   textInverted: "#0d1c30",
+   helperText: "#B6B6B6"
 }
---
+++ app/assets/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+    light: {
+        background: "#F7F7F7",
+        main: "#FF2782",
+        text: "white",
+        textInverted: "#0d1c30",
+        helperText: "#B6B6B6"
+    },
+    dark: {
+        background: "#18191A",
+        main: "#FF2782",
+        text: "#242526",
+        textInverted: "white",
+        helperText: "#B6B6B6"
+    }
+}
+
+export default theme;
