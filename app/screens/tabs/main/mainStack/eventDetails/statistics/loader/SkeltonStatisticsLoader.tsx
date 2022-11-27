f2cae38127c331d1096ccfa839c0ed1f4ea4b06d
abdenassar01 <abdenassaramimi99@gmail.com>
2022-11-27 21:20:11 +0100

    update loader style👌👨‍🎨

--- app/screens/tabs/main/mainStack/eventDetails/statistics/loader/SkeltonStatisticsLoader.tsx
+++ app/screens/tabs/main/mainStack/eventDetails/statistics/loader/SkeltonStatisticsLoader.tsx
@@ -5,9 +5,9 @@ function SkeltonStatisticsLoader(props: any) {
   return (
     <SkeletonPlaceholder backgroundColor={ props.theme.background } highlightColor={ props.theme.helperText }>
       <SkeletonPlaceholder.Item alignItems="center" marginTop={ 20 }>
-        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 50 } />
-        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 50 } />
-        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 50 } />
+        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 10 } />
+        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 10 } />
+        <SkeletonPlaceholder.Item width="95%" height={ 40 } margin={ 5 } borderRadius={ 10 } />
       </SkeletonPlaceholder.Item>
     </SkeletonPlaceholder>
   )
