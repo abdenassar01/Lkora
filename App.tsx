d6477ade0f27861114f531b13821757d46c7d84d
abdenassar01 <abdenassaramimi99@gmail.com>
2022-12-07 22:56:46 +0100

    presiste theme value to db🔋🎩

--- App.tsx
+++ App.tsx
@@ -1,25 +1,56 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {
+  Alert,
   StatusBar,
 } from 'react-native';
 import TabNavigator from './app/screens/tabs/TabNavigator';
 import { ThemeProvider } from 'styled-components';
 import { light, dark } from './app/assets/theme';
 import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 const queryClient = new QueryClient()
 
 const App = () => {
 
-  const [ theme, setTheme ] = useState("light")
+  const [ theme, setTheme ] = useState<string>("light")
+
+  const storeTheme = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('@theme', value)
+    } catch (e) {
+      Alert.alert("Error presesting theme value");
+    }
+  }
+
+
+  const getTheme = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@theme')
+      if(value !== null) {
+        return value;
+      }
+    } catch(e) {
+      Alert.alert("Error reading theme value");
+    }
+  }
 
   const handleThemeToggle = () => {
     if (theme === "light") {
       setTheme("dark");
+      storeTheme("dark");
       return;
     }
     setTheme("light");
+    storeTheme("light");
+  }
+
+  if(getTheme() != undefined){
+    getTheme().then(theme => theme != undefined && setTheme(theme)).catch(e => Alert.alert("Error"))
+  }else{
+    storeTheme("light")
   }
 
   return (
