788434404688c7ca14aaa41c9a86898377bf618c
abdenassar01 <abdenassaramimi99@gmail.com>
2022-12-07 23:48:56 +0100

    adding incidents structures🤩🃏

--- app/screens/tabs/main/mainStack/eventDetails/statistics/sections/incidents/styles/Styles.ts
+++ app/screens/tabs/main/mainStack/eventDetails/statistics/sections/incidents/styles/Styles.ts
@@ -1,6 +1,27 @@
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import styled from 'styled-components';
+import { MainText } from "../../../../../../../../../assets/text/Text";
 
 export const IncidentWrapper = styled(View)`
   
+`
+
+export const IncidentPeriod = styled(View)`
+
+`
+  
+export const IncidentItem = styled(View)`
+
+`
+
+export const PlayerInText = styled(MainText)`
+
+`
+
+export const PlayerOutText = styled(MainText)`
+
+`
+
+export const IncidentLogo = styled(Image)`
+
 `
