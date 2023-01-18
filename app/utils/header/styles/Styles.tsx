61a9c8275470b337d8fa22d12cc50e6815a32839
abdenassar01 <abdenassaramimi99@gmail.com>
2023-01-18 01:29:10 +0100

    fix theme not getting not dynamic in some screens👨‍🎓🖖

--- app/utils/header/styles/Styles.tsx
+++ app/utils/header/styles/Styles.tsx
@@ -1,6 +1,5 @@
 import { Image, TouchableOpacity, View } from "react-native";
 import styled from "styled-components";
-import { COLOR } from "../../../assets/color";
 import { MainText } from "../../../assets/text/Text";
 
 export const HeaderBackground = styled(View)`
@@ -33,7 +32,7 @@ export const GoBack = styled(TouchableOpacity)`
 export const TournamentTitle = styled(MainText)`
     font-size: 22px;
     font-weight: 600;
-    color: ${ COLOR.main };
+    color: ${ props => props.theme.main };
     position: absolute;
     right: 10px;
 `
--- app/utils/header/withgoback/WithGoBackHeader.tsx
+++ app/utils/header/withgoback/WithGoBackHeader.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { GoBack, HeaderWrapper, TournamentTitle } from "../styles/Styles";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { COLOR } from "../../../assets/color";
+import { useTheme } from "styled-components";
 
 type Props = {
   tournament: string 
@@ -10,11 +10,12 @@ type Props = {
 export default function WithGoBackHeader({ tournament }: Props) {
 
     const navigation = useNavigation();
+    const theme = useTheme();
 
   return (
     <HeaderWrapper>
         <GoBack onPress={ () => navigation.goBack() }>
-            <Ionicons name="chevron-back-circle-sharp" size={ 35 } color={ COLOR.main } />
+            <Ionicons name="chevron-back-circle-sharp" size={ 35 } color={ theme.main } />
         </GoBack>
         <TournamentTitle>
           { tournament ?? "loading..." }
